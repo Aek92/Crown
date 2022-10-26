49525fdeb658471165b61e56b07fe7dc0f9fd7f7
Aek92 <aek92.ak@gmail.com>
2022-10-26 12:34:40 +0200

    Added stripe payment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePropmise } from './utils/stripe/stripe.utils';
 
 import App from "./App";
 
@@ -16,7 +18,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePropmise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,13 +1,21 @@
-import {useSelector} from 'react-redux'
-import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
+import { useSelector } from "react-redux";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../store/cart/cart.selector";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
-import {CheckoutContainer, CheckoutHeader, HeaderBlock, Total } from "./checkout.styles";
+import PaymentForm from '../../components/payment-form/payment-form.component';
+import {
+  CheckoutContainer,
+  CheckoutHeader,
+  HeaderBlock,
+  Total,
+} from "./checkout.styles";
 
 const Checkout = () => {
-
-const cartItems = useSelector(selectCartItems);
-const cartTotal = useSelector(selectCartTotal);
+  const cartItems = useSelector(selectCartItems);
+  const cartTotal = useSelector(selectCartTotal);
 
   return (
     <CheckoutContainer>
@@ -32,6 +40,7 @@ const cartTotal = useSelector(selectCartTotal);
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };
